ed55252ef0fcdc6080c615fb6230c5ac2d694e39
Victor Ramirez <valzkat12@gmail.com>
2023-07-18 20:04:13 -0500

    clase 3

--- calculadora/src/paquete2/ClaseTres.java
+++ calculadora/src/paquete2/ClaseTres.java
@@ -8,40 +8,44 @@ public class ClaseTres {
 
 	
 	
-	public static void main(String[] args) {
-		ClasePrincipal objClasePPal=new ClasePrincipal();
-		String opcion=objClasePPal.menu();
-		int op= objClasePPal.convertirEntero(opcion);
-		while(op<=0) {
-			//Customizar el mensaje desde la segunda solucitud de menu.
-			 opcion = objClasePPal.menu();
-			 op= objClasePPal.convertirEntero(opcion);
-		}
-		
-		String nume1=JOptionPane.showInputDialog("Por favor ingrese el primer numero a operar");
-		int num1 = objClasePPal.convertirEntero(nume1);
-		//VAlidacion datos numericos
-		String nume2=JOptionPane.showInputDialog("Por favor ingrese el segundo numero a operar");
-		int num2 = objClasePPal.convertirEntero(nume2);
-		//VAlidacion datos numericos
-		// Si es division no cero
-		
-		double retorno=0;
-		switch (op) {
-		case 1: 
-			retorno= num1+num2;
-			break;
-		case 2: 
-			retorno= num1-num2;
-			break;
-		case 3: 
-			retorno= num1*num2;
-			break;
-		case 4: 
-			retorno= num1/num2;
-			break;
-		default:
-			throw new IllegalArgumentException("Unexpected value: " + op);
+public static void main(String[] args) {
+	ClasePrincipal objClasePPal=new ClasePrincipal();
+	String opcion=objClasePPal.menu();
+	int op= objClasePPal.convertirEntero(opcion);
+	while(op<=0) {
+		//Customizar el mensaje desde la segunda solucitud de menu.
+		 opcion = objClasePPal.menu();
+		 op= objClasePPal.convertirEntero(opcion);
+	}
+	
+ while(op<5) {	
+	String nume1=JOptionPane.showInputDialog("Por favor ingrese el primer numero a operar");
+	while(!objClasePPal.esNumerico(nume1)) {
+		nume1=JOptionPane.showInputDialog("Por favor ingrese el primer numero a operar(Debe ser datos numericos)");
+	}	
+	double num1 = Double.parseDouble(nume1); 
+	//VAlidacion datos numericos
+	String nume2=JOptionPane.showInputDialog("Por favor ingrese el segundo numero a operar");
+	while(!objClasePPal.esNumerico(nume2)) {
+		nume2=JOptionPane.showInputDialog("Por favor ingrese el segundo numero a operar (Debe ser dato numerico)");
+	}
+	double num2 = Double.parseDouble(nume2);
+	//VAlidacion datos numericos
+	// Si es division no cero
+	
+	// mostrar mensaje segun el caso( letras o cero)
+	if(op==4) {
+		while(num2==0) {
+			nume2=JOptionPane.showInputDialog("Por favor ingrese el segundo numero a operar (No esta permitido en cero como denominador)");
+			if(objClasePPal.esNumerico(nume2)) {
+				num2=Double.parseDouble(nume2);
+			}else {
+				nume2=JOptionPane.showInputDialog("Por favor ingrese el segundo numero a operar (No letras)");	
+				if(objClasePPal.esNumerico(nume2)) {
+					num2=Double.parseDouble(nume2);
+				}
+				
+			}
 		}
 		
 	}
