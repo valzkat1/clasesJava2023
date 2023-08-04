05999fdb3c92cbf9b451298146a59003b3acd562
Victor Ramirez <valzkat12@gmail.com>
2023-08-03 20:32:04 -0500

    Update MarcoImagen.java

--- Marcos&Ventanas/src/marcos/MarcoImagen.java
+++ Marcos&Ventanas/src/marcos/MarcoImagen.java
@@ -1,13 +1,31 @@
 package marcos;
 
-public class MarcoImagen {
+import javax.swing.JEditorPane;
+import javax.swing.JFrame;
 
+public class MarcoImagen extends JFrame{
+
+	JEditorPane panelEditar;
+	
 	public MarcoImagen() {
-		// TODO Auto-generated constructor stub
+		
+		panelEditar= new JEditorPane();
+		panelEditar.setContentType("text/html");
+		panelEditar.setText("<html><body>Contenido</body></html>");
+		panelEditar.setEditable(true);
 	}
 
+ public void iniciarMarco() {
+		
+		getContentPane().setLayout(null);
+		setTitle("Marco con Imagen");
+		setSize(300,200);
+		setVisible(true);
+		
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
