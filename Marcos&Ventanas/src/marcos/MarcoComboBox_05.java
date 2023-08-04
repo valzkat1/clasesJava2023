46110afc1856f8e4e293d291c85bddc53073ce63
Victor Ramirez <valzkat12@gmail.com>
2023-08-03 19:57:45 -0500

    ClaseAgosto 3

--- Marcos&Ventanas/src/marcos/MarcoComboBox_05.java
+++ Marcos&Ventanas/src/marcos/MarcoComboBox_05.java
@@ -3,8 +3,11 @@ package marcos;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
-public class MarcoComboBox_05 extends JFrame{
+public class MarcoComboBox_05 extends JFrame implements ItemListener{
 
 	private JComboBox combo1;
 	private JLabel label1;
@@ -13,23 +16,26 @@ public class MarcoComboBox_05 extends JFrame{
 	public MarcoComboBox_05() {
 		
 		combo1 = new JComboBox();
+		combo1.setBackground(new Color(192, 192, 192));
 		combo1.setBounds(20,20,100,30);
-		add(combo1);
-		
+		getContentPane().add(combo1);
+		combo1.addItem("-- Seleccionar --");
 		combo1.addItem("Opcion 1");
 		combo1.addItem("Opcion 2");
 		combo1.addItem("Opcion 3");
 		combo1.addItem("Opcion 4");
 		
+		combo1.addItemListener(this);
+		
 		label1 = new JLabel("Selecciona una Opción");
-		label1.setBounds(20,70,150,30);
-		add(label1);
+		label1.setBounds(20,70,200,30);
+		getContentPane().add(label1);
 		iniciarMarco();
 	}
 
 	public void iniciarMarco() {
 		
-		setLayout(null);
+		getContentPane().setLayout(null);
 		setTitle("Marco con ComboBox");
 		setSize(300,200);
 		setVisible(true);
@@ -40,6 +46,25 @@ public class MarcoComboBox_05 extends JFrame{
 		// TODO Auto-generated method stub
 
 		new MarcoComboBox_05();
+	}
+
+	@Override
+	public void itemStateChanged(ItemEvent e) {
+
+		if(e.getSource()==combo1) {
+			
+			String itemSeleccionado = (String) combo1.getSelectedItem();
+			// Clase String y sus metodos.
+			//String arregloCadena[] = itemSeleccionado.split("");
+			if(itemSeleccionado.equals("-- Seleccionar --")) {
+				label1.setText("Por favor seleccione una opcion");
+			}else
+			 label1.setText("Ha seleccionado: "+itemSeleccionado);
+			
+			
+		}
+		
+		
 	}
 
 }
---
+++ Marcos&Ventanas/src/marcos/MarcoImagen.java
@@ -0,0 +1,14 @@
+package marcos;
+
+public class MarcoImagen {
+
+	public MarcoImagen() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
