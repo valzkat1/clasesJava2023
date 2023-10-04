f6fc9b41ef48c4916587bec627cb647bf49dfaf6
Victor Ramirez <valzkat12@gmail.com>
2023-10-03 20:51:27 -0500

    Update Controlador.java

--- clasesJava2023_2/agenda_java/src/main/java/com/fundacionview/controladores/Controlador.java
+++ clasesJava2023_2/agenda_java/src/main/java/com/fundacionview/controladores/Controlador.java
@@ -18,9 +18,7 @@ import java.util.Collection;
 
 /**
  *
- * @author Charly Cimino Aprendé más Java en mi canal:
- * https://www.youtube.com/c/CharlyCimino Encontrá más código en mi repo de
- * GitHub: https://github.com/CharlyCimino
+ * @author Victor Ramirez
  */
 public class Controlador {
 
